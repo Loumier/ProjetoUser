eb077c3e2692a76e85791343a1ed51e8849d798b
Guilherme <loumier28@gmail.com>
2020-09-21 00:53:59 -0300

    Aula de sistemas distribuidos

--- src/config/config.js
+++ src/config/config.js
@@ -2,12 +2,12 @@
 module.exports = {
     development: {
         database: {
-            host: 'ls-20024416c34ba6dbfa3923d3f3f120e6e3a030c7.ceefo25bexkd.us-east-1.rds.amazonaws.com',
+            host: 'ls-4923b76508d53a05deca2280aa493ca4e2617b60.cj6jfxqol2pc.us-east-1.rds.amazonaws.com',
             port: 3306,
             name: 'db_usuario',
             dialect: 'mysql',
-            user: 'dbmasteruser',
-            password: '-.CCUH04CY5Nt9H?cNb^$,`ertl(5j{R'
+            user: 'admin',
+            password: 'bT2033[:#I-`~*G.42}~hp?0I)aC`0$a'
         }
     },
     production: {
